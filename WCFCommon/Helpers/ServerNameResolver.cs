7c0f6df0e7f5feb55c7ebbe50d85e8dd645cec42
Andrey Nasonov <andrey.nasonov@actonica.ru>
2020-10-29 17:35:42 +0700

    Added server name resolver

---
+++ WCFCommon/Helpers/ServerNameResolver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFCommon.Helpers
+{
+    public static class ServerNameResolver
+    {
+        private static readonly string _configFileName = "config.txt";
+        internal static string GetConfigPath() 
+        {
+            var codebaseUri = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+            var parentFolder = Path.GetDirectoryName(codebaseUri.LocalPath);
+            var configPath = Path.Combine(parentFolder, _configFileName);
+            return configPath;
+        }
+
+        public static void SaveServerName(string serverName = null)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(GetConfigPath()))
+                {
+                    sw.WriteLine(serverName ?? Dns.GetHostName());
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+            }
+        }
+
+        public static string GetServerName()
+        {
+            try
+            {
+                var serverName = string.Empty;
+                using (var sr = new StreamReader(GetConfigPath()))
+                {
+                    serverName = sr.ReadToEnd();
+                }
+                return serverName;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
