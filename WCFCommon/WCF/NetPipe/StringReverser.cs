645a526d0367c7ff3027b4cdd98d2459688ebcc8
Andrey Nasonov <andrey.nasonov@actonica.ru>
2020-11-25 10:08:19 +0700

    Check that each client have separate class instances on host side.

--- WCFCommon/WCF/NetPipe/StringReverser.cs
+++ WCFCommon/WCF/NetPipe/StringReverser.cs
@@ -8,8 +8,12 @@ namespace WCFCommon.WCF.NetPipe
 {
     public class StringReverser : IStringReverser
     {
+        private string _lastString;
+
         public string ReverseString(string value)
         {
+            _lastString = value;
+
             char[] retVal = value.ToCharArray();
             int idx = 0;
             for (int i = value.Length - 1; i >= 0; i--)
